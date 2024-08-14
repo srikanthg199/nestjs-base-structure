512632a4a3ac92142ad199951d7fcd194df4cc42
srikanth019 <srikanth.golla@brainvire.com>
2024-08-14 15:15:01 +0530

    Logger utilization

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
+  logger.log('Server listening on port 3000');
 }
 bootstrap();
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,14 +1,15 @@
 import {
   Injectable,
   InternalServerErrorException,
+  Logger,
   NotFoundException,
 } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-
 @Injectable()
 export class UserRepository extends Repository<User> {
+  private logger = new Logger('UserRepository');
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
   }
@@ -18,6 +19,7 @@ export class UserRepository extends Repository<User> {
       const user = this.create(createUserDto);
       await this.save(user); // Use save to persist the user
     } catch (error) {
+      this.logger.error(`Failed to create user: ${error.message}`, error.stack);
       throw new InternalServerErrorException('Failed to create user');
     }
   }
@@ -26,7 +28,7 @@ export class UserRepository extends Repository<User> {
     try {
       return await this.findOne(filter);
     } catch (error) {
-      console.log(error);
+      this.logger.error(`Failed to get user: ${error.message}`, error.stack);
       throw new NotFoundException('user not found');
     }
   }
@@ -35,6 +37,10 @@ export class UserRepository extends Repository<User> {
     try {
       return await this.find(filter);
     } catch (error) {
+      this.logger.error(
+        `Failed to retrieve users: ${error.message}`,
+        error.stack,
+      );
       throw new InternalServerErrorException('Failed to retrieve users');
     }
   }
@@ -43,7 +49,7 @@ export class UserRepository extends Repository<User> {
     try {
       return await this.delete(filter);
     } catch (error) {
-      // handle error if user not found or any other issues
+      this.logger.error('Failed to delete user', error);
       throw new InternalServerErrorException('Failed to delete user');
     }
   }
@@ -52,7 +58,7 @@ export class UserRepository extends Repository<User> {
     try {
       return await this.update(filter, updateData);
     } catch (error) {
-      // handle error if user not found or any other issues
+      this.logger.error('Failed to update user', error);
       throw new InternalServerErrorException('Failed to update user');
     }
   }
